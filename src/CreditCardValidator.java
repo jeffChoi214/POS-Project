18a0c2d01f7675d91872665854e694d054be723e
w3iiipu <w3iiipu@gmail.com>
2017-02-16 00:17:38 -0500

    added credit card/check validation

---
+++ src/CreditCardValidator.java
@@ -0,0 +1,132 @@
+import java.util.Scanner;
+
+public class CreditCardValidator {
+
+//use to take cc# as long and check as valid numbers input
+    public static long getLong(Scanner sc, String prompt) {
+        long l = 0;
+        boolean isValid = false;
+        while (isValid == false) {
+            System.out.print(prompt);
+            if (sc.hasNextLong()) {
+                l = sc.nextLong();
+                isValid = true;
+            } else {
+                System.out.println("Error! Invalid long value. Try again.");
+            }
+            sc.nextLine(); //clear buffer
+        }
+        return l;
+    }
+
+//checks digits length and prefixes for types of card
+    public static String isValidCardNumber(Scanner sc, String prompt) {
+        String lastFourDigits = "";
+        String cardType = "";
+        boolean condition = true;
+        while (condition) {
+            System.out.println(prompt);
+            long cardNumber = getLong(sc, "");
+            int length = String.valueOf(cardNumber).length();
+
+            String cardDigits = String.valueOf(cardNumber);
+            String digit1 = cardDigits.substring(0,1);
+            String digit2 = cardDigits.substring(0,2);
+
+            // MasterCard have 16 digits length and prefix 51 to 55
+            // Visa have 13 or 16 digits length and prefix 4
+            // Discover have 16 digits length prefix of 6011
+            // Amex have 15 digits length and prefix of 34 or 37
+
+            if (digit1.equals("4") && ((length == 13) || (length == 16))) {
+                lastFourDigits = new Long(cardNumber).toString(12, 15);
+                condition = false;
+                cardType = "Visa";
+            } else if ((digit2.compareTo("51") >= 0 && digit2.compareTo("55") <= 0) && (length == 16)) {
+                lastFourDigits = new Long(cardNumber).toString(12, 15);
+                condition = false;
+                cardType = "Master Card";
+            } else if (digit2.equals("60") && (length == 16)) {
+                lastFourDigits = new Long(cardNumber).toString(12, 15);
+                condition = false;
+                cardType = "Discover";
+            } else if ((digit2.equals("34") || digit2.equals("37")) && (length == 15)) {
+                lastFourDigits = new Long(cardNumber).toString(12, 14);
+                condition = false;
+                cardType = "American Express";
+            } else
+                System.out.println("That is not a valid card number. Try again.");
+        }
+        return lastFourDigits;  //returns the last 4 digits for confirmation
+    }
+
+//checks the date enter by format of MMyy and checks it against last date of previous month
+    public static boolean isValidCardExpiration(String s){
+
+        java.text.DateFormat sdf = new java.text.SimpleDateFormat("MMyy");
+
+        java.util.Calendar now = java.util.Calendar.getInstance();
+        now.set(now.get(java.util.Calendar.YEAR), now.get(java.util.Calendar.MONTH), 0, 23, 59, 59);
+
+        try {
+            java.util.Date exp = sdf.parse(s);
+
+            if (exp.before(now.getTime()))
+                return false;
+        } catch (java.text.ParseException e) {
+            return false;
+        }
+        return true;
+    }
+
+//simple validation that checks by length of string. any 4 character will pass the validation.
+    public static void isValidCardDate(Scanner sc, String prompt) {
+        boolean condition = true;
+        while (condition) {
+            System.out.println(prompt);
+            long cardNumber = getLong(sc, "");
+            int length = String.valueOf(cardNumber).length();
+            if (length == 4) {
+                condition = false;
+            } else
+                System.out.println("That is not a valid expiration date. Try again.");
+        }
+    }
+
+//Checking CVV by String length. any characters including letters will pass validation
+    public static void isValidCardCVV(Scanner scan, String prompt) {
+        boolean condition = true;
+        while (condition) {
+            System.out.println(prompt);
+            long cardNumber = getLong(scan, "");
+            int length = String.valueOf(cardNumber).length();
+            if (length == 3) {
+                condition = false;
+            } else
+                System.out.println("That is not a valid CVV number. Try again.");
+        }
+    }
+
+//Currently unable to differentiate 3 digits vs 4 digits CVV
+    public static int isValidCVV(Scanner sc, String prompt)
+    {
+        int i = 0;
+        boolean condition = true;
+        while (condition == true)
+        {
+            System.out.print(prompt);
+            i = sc.nextInt();
+            sc.nextLine();
+            if ( i <= 9999){
+                condition = false;
+            } else {
+                System.out.println("That is not a valid CVV number. Try again. ");
+            }
+
+        }
+
+        return i;
+    }
+
+
+} //end of class
---
+++ src/InputValidator.java
@@ -0,0 +1,87 @@
+import java.util.Scanner;
+
+public class InputValidator {
+
+    public static String getString(Scanner sc, String prompt)
+    {
+        System.out.print(prompt);
+        String s = sc.nextLine();
+
+        return s;
+    }
+
+
+    public static int getInt(Scanner sc, String prompt)
+    {
+        int i = 0;
+        boolean isValid = false;
+        while (isValid == false)
+        {
+            System.out.print(prompt);
+            if (sc.hasNextInt())
+            {
+                i = sc.nextInt();
+                isValid = true;
+            }
+            else
+            {
+                System.out.println("Error! Invalid integer value. Try again.");
+            }
+            sc.nextLine();  //clear buffer
+        }
+        return i;
+    }
+
+
+    public static int getInt(Scanner sc, String prompt, int min, int max)
+    {
+        int i = 0;
+        boolean isValid = false;
+        while (isValid == false)
+        {
+            i = getInt(sc, prompt);
+            if (i < min)
+                System.out.println(
+                        "Error! Number must be " + min + " or greater.");
+            else if (i > max)
+                System.out.println(
+                        "Error! Number must be " + max + " or less.");
+            else
+                isValid = true;
+        }
+        return i;
+    }
+
+    public static String isValidRoutingNumber (Scanner sc, String prompt) {
+        System.out.println(prompt);
+        String routingNumber = sc.nextLine();
+        if(routingNumber.length()<9||routingNumber.length()>9)
+            System.out.println("Invalid routing number.");
+        return routingNumber;
+    }
+
+    public static String isValidCheckNumber(Scanner scan, String prompt) {
+        System.out.println(prompt);
+        String checkNumber = scan.nextLine();
+        if (checkNumber.length() < 4 || checkNumber.length() > 4)
+            System.out.println("Invalid check number.");
+        return checkNumber;
+    }
+
+    public static String isValidAccountNumber(Scanner scan, String prompt) {
+        System.out.println(prompt);
+        String accountNumber = scan.nextLine();
+        if (accountNumber.length() < 14 || accountNumber.length() > 14)
+            System.out.println("Invalid account number.");
+        return accountNumber;
+    }
+
+
+
+
+
+
+
+
+
+}
